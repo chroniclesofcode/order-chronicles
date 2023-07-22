da91557edd48dda13489b7738ba51324febf1bea
Donald Tang <donald.tang.email@googlemail.com>
2023-07-22 15:18:01 +1000

    orders seem to execute properly

--- include/orderbook/LOBSTER/MessageParser.h
+++ include/orderbook/LOBSTER/MessageParser.h
@@ -15,7 +15,12 @@ public:
         }  else if (o.event_type == 3) {
             orders.removeOrder(o.id);
         }  else if (o.event_type == 4) {
-
+            /* This occurs when the orderbook indicates that a trade has occured
+               for a certain entry in the orderbook. It does not give us the
+               entry that caused the match, so we create it ourselves to
+               imitate this functionality */
+            Order opp(o.price, o.quantity, !o.direction, ID_MAX, 1, o.time);
+            orders.addOrder(opp);
         }  else if (o.event_type == 5) {
 
         }  else if (o.event_type == 6) {
@@ -23,7 +28,7 @@ public:
         }  else if (o.event_type == 7) {
 
         }
-        orders.LOBSTERoutput(1);
+        //orders.LOBSTERoutput(1);
     }
 
     static Order parseMessage(const std::string& s) {
@@ -47,7 +52,7 @@ public:
         int odir = stoi(s.substr(j, i));
         odir = odir == 1 ? 1 : 0;
 
-        std::cout << "time: " << otime << " etype: " << eventtype << " id: " << oid << " sz: " << osize << " price: " << oprice << " dir " << odir << std::endl;
+        //std::cout << "time: " << otime << " etype: " << eventtype << " id: " << oid << " sz: " << osize << " price: " << oprice << " dir " << odir << std::endl;
         return Order(oprice, osize, odir, oid, eventtype, otime);
     }
 private:
@@ -60,4 +65,5 @@ private:
     }
 
     Orderbook &orders;
+    int ID_MAX = std::numeric_limits<int>::max();
 };
